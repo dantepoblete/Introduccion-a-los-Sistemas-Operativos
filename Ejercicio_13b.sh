6f2c095349eb0678ad363e75b074de62d2e8e021
dantepoblete <62101433+dantepoblete@users.noreply.github.com>
2020-10-26 20:59:00 -0300

    Ejercicio de la Práctica 3

---
+++ Ejercicio_13b.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Seleccione una opción:"
+echo "(a)-Listar."
+echo "(b)-DondeEstoy."
+echo "(c)-QuienEsta"
+read opcion
+if [ $opcion == 'a' ] then
+  ls-a
+elif [ $opcion == 'b' ] then
+  pwd
+elif [ $opcion == 'c' ] then
+  users
+else
+  echo "Opción errónea."
+exit
