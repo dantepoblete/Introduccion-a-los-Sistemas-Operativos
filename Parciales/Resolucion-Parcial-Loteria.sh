d995854be0963729d53529a8dcc113d13abf072a
dantepoblete <62101433+dantepoblete@users.noreply.github.com>
2020-11-20 02:47:40 -0300

    Resolucion Parcial Loteria

--- Parciales/Resolucion-Parcial-Loteria.sh
+++ Parciales/Resolucion-Parcial-Loteria.sh
@@ -7,20 +7,21 @@ numval='^[1-900]+$'   #Representa al rango de enteros positivos entre el 1 y 900
 if ! [[ $1 =~ $numval ]];then   #Verifico que el valor del parámetro sea un número entero positivo.
   echo "INVALID PARAMETER"
   exit 1
-fi  
+fi
+#Inicializo las variables:
 fecha1=""
 fecha2=""
 cant=0
 for linea in $(cat 2013_2020.csv); do   #Abro el archivo csv y trabajo con cada una de sus líneas.
-  suma=0
-  for (( i=2; i<=7; i++ )) do   #Para cada linea del archivo trabajo con el rango de columnas de 2 a 7 ya que contienen los valores que necesito.
+  suma=0    #Para cada linea voy a sumar los valores de las combinaciones ganadoras.
+  for (( i=2; i<=7; i++ )) do   #Para cada linea del archivo trabajo con el rango de columnas de 2 a 7 ya que contienen los valores de las combinaciones ganadoras.
       num=$(echo $linea | cut -d "," -f$i)  #Obtengo el numero correspondiente a la columna i.
       let suma=$suma+10#$num
   done
-  if [ $suma -eq $1 ]; then   #Asigno la fecha de la primer ocurrencia.
+  if [ $suma -eq $1 ]; then   #Si la suma de los valores coincide con el parámetro ingresado:
     let cant=$cant+1    #Actualizo la cantidad de ocurrencias.
     if [ "$fecha1" == "" ];then
-      fecha1=$(echo $linea | cut -d "," -f1)
+      fecha1=$(echo $linea | cut -d "," -f1)   #Asigno la fecha de la primer ocurrencia.
     else
       fecha2=$(echo $linea | cut -d "," -f1)    #Si la primer ocurrencia ya se encuentra asignada, solo actualizo la ultima ocurrencia procesada.
     fi
